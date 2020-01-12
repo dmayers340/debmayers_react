1e3e4a7832d4795792fe3fd4a7956c6ee6f323a7
Deb <dmayers340@gmail.com>
2020-01-12 16:02:06 +0000

    Updated 404 page

--- src/Components/Common/UnderConstruction.js
+++ src/Components/Common/UnderConstruction.js
@@ -4,8 +4,8 @@ export const UnderConstruction = () => {
     return(
         <>
             <h1>404</h1>
-            <p> Oh no! The page you are digging for does not exist :( Either go back a thousand years to see it or look elsewhere</p>
-            <img src= "/img/Mithraeum_Front.jpg" alt="Front of the Temple to Mithras at Carrawburgh"/>
+            <p> Oh no! The page you are searching for does not exist</p>
+            <img src= "/img/mithraeum_reconstructed.jpg" alt="Recreated of the Temple to Mithras at Carrawburgh with the cult statue displaying Mithras killing the bull"/>
         </>
     )
 }
