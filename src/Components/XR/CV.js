db9527eb0ce1eb11c679dfe55860f52092f7d6e2
Deb <dmayers340@gmail.com>
2020-01-25 18:49:19 +0000

    Changed to glb

--- src/Components/XR/CV.js
+++ src/Components/XR/CV.js
@@ -36,12 +36,13 @@ export const CV = () => {
                     gearvr-controls="right">
                 </a-entity>
 
-                <a-text color="black" position="-4.923 0.949 -5.669" value="Work in progress, Mayers CV. Walk around to see some of the work that I have done."></a-text> 
+                <a-text color="black" position="-3.62 0.949 -5.669" value="Work in progress, Mayers CV. Walk around to see some of the work that I have done."></a-text> 
                 <a-gltf-model src="models/carrawburgh/carrawburgh.gltf" scale="0.01 0.01 0.01" position="0 0 -44.87"></a-gltf-model>
                 <a-obj-model src="models/mural/mural.obj"  mtl="models/mural/mural.mtl" scale=".09 .09 .09" position="2.476 1.083 -5.547" rotation="4.645 0 0" ></a-obj-model>
                 <a-obj-model src="models/lion/lion.obj" scale=".06 .06 .06" mtl="models/lion/lion.mtl" position="9.891 2.128 4.265" rotation="-22.9 73.96 -93"></a-obj-model>
-
-            </a-scene>
+                <a-obj-model src="models/mithras/mithras.obj" mtl="models/mithras/mithras.mtl" position="0 0 -7.172" rotation="0 0 -91.72" scale="0.1 0.1 0.1"></a-obj-model>
+                <a-gltf-model src="models/prima-porta/PrimaPortaGLB.glb" scale="0.1 0.1 0.1" position="-4.74 0.843 -7.015" rotation="0 104 -88.17"></a-gltf-model>
+            </a-scene>models
         </div>
     )
 }
