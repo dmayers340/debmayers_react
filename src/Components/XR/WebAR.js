cfca87ae9b20c1828edf2f7f118a2d420bfbb012
Deb <dmayers340@gmail.com>
2020-02-01 08:33:10 +0000

    Added model to test

--- src/Components/XR/WebAR.js
+++ src/Components/XR/WebAR.js
@@ -10,6 +10,8 @@ export const WebAR = () => {
         <div className = 'arcode'>
             <h1>Testing WebAR</h1>
             <a className = "embed8-link" href = "https://8th.io/hykve" data-attached="true">Click here to see the (re)created altars from the Carrawburgh Temple to Mithras on Project 8th wall</a>
+            
+            <a href="./AR.html">Testing AR.JS</a>        
         </div>
     )
 }
