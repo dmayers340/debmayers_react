e6d4b068e01be529eed6fe89d6ed0b2518270e28
Deb <dmayers340@gmail.com>
2020-02-05 20:24:43 +0000

    Added new glb to try and test ground texture, updated packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,7 +10,11 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: "babel-loader" 
+      }
     ]
   }
 };
